188efcd05446391b8b7fc65abf779670e1a01506
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-24 14:04:01 -0800

    adding in new columns to the celebs table

---
+++ Section-01/07-update.sql
@@ -0,0 +1,6 @@
+ALTER TABLE celebs
+ADD COLUMN
+twitter_handle TEXT;
+
+SELECT * FROM CELEBS;
+-- Added a column to the table called twitter_handle that takes in only text inputs, but all of the values in that column are null.
