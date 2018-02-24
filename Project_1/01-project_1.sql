f568626a5b4303191bc1c54988681933f43e2f03
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-24 15:05:48 -0800

    more progress with updating the table rows with their respective emails

--- Project_1/01-project_1.sql
+++ Project_1/01-project_1.sql
@@ -17,17 +17,37 @@ VALUES(3, 'The Weeknd', '1990-02-16');
 
 INSERT INTO friends (id, name, birthday)
 VALUES (4, 'Kobe Bryant', '1978-08-23');
--- SELECT * FROM friends; [worked]
+-- SELECT * FROM friends; [worked!]
 
 
 -- Jane Doe just got married! Her new last name is Smith. Update her record in your database.
 UPDATE friends
 SET name = 'Jane Smith'
 WHERE id = 1;
--- SELECT * FROM friends; [worked]
+-- SELECT * FROM friends; [worked!]
 
 
 -- Add a new column to your table named email.
 ALTER TABLE friends
 ADD COLUMN
 email TEXT;
+-- SELECT * FROM friends; [worked!]
+
+
+-- Update the email address for each friend in your table. Jane Doe's email is jdoe@example.com.
+UPDATE FRIENDS
+SET email = 'jdoe@example.com'
+WHERE id = 1;
+
+UPDATE friends
+SET email = 'biggie@datflowdoe.com'
+WHERE id = 2;
+
+UPDATE friends
+SET email = 'falsetto@errday.com'
+WHERE id = 3;
+
+UPDATE friends
+SET email = 'blackmamba@baller.com'
+WHERE id = 4;
+SELECT * FROM friends; [worked!]
