3b44e37ec9716ba6ed50586a8e47251b370527cd
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-24 13:58:44 -0800

    more insert into practice

---
+++ Section-01/05-insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO celebs (id, name, age) VALUES
+(2, 'Beyonce Knowles', 33);
+
+INSERT INTO celebs (id, name, age) VALUES
+(3, 'Jeremy Lin', 26);
+
+INSERT INTO celebs (id, name, age) VALUES
+(4, 'Taylor Swift', 26);
+
+SELECT name FROM celebs; -- returns all the names in the celebs table
+-- Justin Bieber
+-- Beyonce Knowles
+-- Jeremy Lin
+-- Taylor Swift
