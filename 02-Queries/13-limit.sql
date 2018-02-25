3d930a14eda2bd2d8982f96b2903d0655084c3ea
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-24 22:54:17 -0800

    working with LIMIT operations with query

---
+++ 02-Queries/13-limit.sql
@@ -0,0 +1,15 @@
+-- LIMIT
+
+SELECT *
+FROM movies
+LIMIT 5;
+
+-- LIMIT is a clause that lets you specify the maximum number of rows the result set will have. This saves space on our screen and makes our queries run faster.
+-- LIMIT always goes at the very end.
+
+--------------------
+-- Combining your knowledge of LIMIT and ORDER BY, write a query that returns the top three highest rated movies.
+SELECT *
+FROM movies
+ORDER BY imdb_rating DESC;
+LIMIT 3;
