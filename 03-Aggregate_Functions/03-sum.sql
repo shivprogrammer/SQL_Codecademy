b528dea2f1801a81c144aef557268770c90a4442
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-25 00:05:49 -0800

    using SUM in SQL

---
+++ 03-Aggregate_Functions/03-sum.sql
@@ -0,0 +1,13 @@
+-- SUM
+-- SUM is a function that takes the name of a column as an argument and returns the sum of all the values in that column.
+
+-- What is the total number of downloads for all of the apps combined?
+SELECT SUM(downloads)
+FROM fake_apps;
+
+-- This adds all values in the downloads column.
+
+--------------------
+
+SELECT SUM(downloads)
+FROM fake_apps;
