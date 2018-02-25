3d2132a92f3efdd5fb95c18442631f7f3d1ba4f1
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-24 23:17:08 -0800

    intro to aggregate functions in SQL

---
+++ 03-Aggregate_Functions/01-aggregate-functions.sql
@@ -0,0 +1,11 @@
+-- Introduction
+-- COUNT: count the number of values
+-- SUM: add up all of the values in a column
+-- MAX/MIN: get the largest/smallest value
+-- AVG: get the mean for all values in a column
+-- ROUND: round the values in a column
+
+----------
+
+SELECT *
+FROM fake_apps;
